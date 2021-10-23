495cfd15ae99c94e16ff8bc413a372cf89d4b221
MilagrosAlbarengo <92064009+MilagrosAlbarengo@users.noreply.github.com>
2021-10-23 18:55:47 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let Puesto = number : document.getElementById("Puesto");
+if (Puesto === 1) {
+  console.log("Entregar medalla de oro");
+} else {
+  if (Puesto === 2) {
+    console.log("Entregar medalla de plata");
+  } else {
+    if (Puesto ===3) {
+      console.log("Entregar medalla de bronce");
+    } else {
+        console.log("Entregar mencion especial");
+      }
+    }
+    }
+  
